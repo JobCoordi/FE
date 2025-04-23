49e3bfea75e773e7400b85d538e28c809f94c00c
jaehyeongjung <113093247+jaehyeongjung@users.noreply.github.com>
2025-04-23 16:47:26 +0900

    feat: FloatingButton 기능 구현 (다크모드, 상단이동) (#50)
    * chore: 메인페이지 브랜치 생성
    
    * chore: mainPage의 layout 셋팅
    
    * feat: mainpage의 section1 작업과 페이지 기초 셋팅
    
    * chore: 브랜치명 하이픈 오류
    
    * refactor: mock 데이터로 따로 처리
    
    * feat: floatingButton 삽입, 컴포넌트 분리
    
    * chore: lenis 라이브러리 추가
    
    * feat: mainpage  스무스 스크롤링 적용
    
    * chore: gsap 인스톨
    
    * feat: background 애니메이션 추가
    
    * refactor: blur size 축소
    
    * refactor: blur 제거
    
    * feat: RotateSection 추가
    
    * refactor: layout에 floating 버튼 위치
    
    * refactor: mainpage 컴포넌트 리팩토링
    
    * refactor: 결과 페이지 floatinBtn 제거
    
    * refactor: 결과 페이지 floating 버튼제거
    
    * refacor: floating-btn 크기 조정 및 hover 추가
    
    * feat: 다크모드 기능 추가와 각 페이지 색상 수정
    
    * refactor: input placeholder dark시 색상 추가, 위치 수정
    
    * refactor: 다크모드 floatingbutton에서 토글로 대체
    
    * refactor: toggle-size 수정
    
    * refactor: floating-button 상단으로 이동으로 변경
    
    * refactor: div 스크롤 인식 불가로 따로 ref 받아서 page내에 floating-btn 추가
    
    * refactor: layout에서 제거, 각 페이지에 floatingbtn 가져오기
    
    * feat: 메세지가 생길 때마다 하단으로 스크롤링
    
    * refactor: toggle-type 분리
    
    * refactor: floatingButto-type 분리
    
    * chore: homeContent 제거
    
    ---------
    
    Co-authored-by: whdjh <99352927+whdjh@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+import TextSection from '@/components/product/main/TextSeciton';
+import ImageSection from '@/components/product/main/ImageSection';
+import LinkSection from '@/components/product/main/LinkSection';
+import RotateSection from '@/components/product/main/RotateSection';
+import FloatingButton from '@/components/common/FloatingButton';
+
 export default function Home() {
   return (
-    <div>
-      메인페이지
-    </div>
+    <>
+      <main className="bg-white dark:bg-black">
+        <RotateSection />
+        <TextSection key="hero" />
+        <ImageSection key="main" />
+        <LinkSection key="link" />
+        <FloatingButton />
+      </main>
+    </>
   );
 }
--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -1,10 +1,7 @@
-import FloatingButton from "@/components/common/FloatingButton";
-
 export default function Page() {
   return (
     <div>
       결과페이지
-      <FloatingButton />
     </div>
   );
 }

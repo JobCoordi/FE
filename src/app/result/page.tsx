4c50880a2ba9c51634ece3674ca4ff85f7364f8b
jaehyeongjung <titochung625@naver.com>
2025-04-29 11:40:08 +0900

    chore: 코드 충돌 해결

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -1,19 +1,11 @@
 import Card from '@/components/common/Card';
-<<<<<<< HEAD
 import { cardData } from '@/mock/resultCard';
 import Pagination from '@/components/common/Pagination';
-=======
-import {cardData} from '@/mock/resultCard';
->>>>>>> 80a83be (refactor: card- mockData 분리)
 
 export default function Page() {
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <Pagination
-        data={cardData}
-        CardComponent={Card}
-        pageSize={3}
-      />
+      <Pagination data={cardData} CardComponent={Card} pageSize={3} />
     </div>
   );
 }

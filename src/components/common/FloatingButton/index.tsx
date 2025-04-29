d7388e71bd2758215d33e406783715fa7ce2f5df
whdjh <whdjh98@naver.com>
2025-04-29 14:45:48 +0900

    fix: type으로 분리

--- src/components/common/FloatingButton/index.tsx
+++ src/components/common/FloatingButton/index.tsx
@@ -3,7 +3,10 @@
 import { useEffect, useState } from 'react';
 import { FloatingButtonProps } from '@/types/floatingbutton';
 
-export default function FloatingButton({ scrollRef }: FloatingButtonProps) {
+export default function FloatingButton({ 
+  type,
+  scrollRef
+}: FloatingButtonProps) {
   const [show, setShow] = useState(false);
 
   useEffect(() => {
@@ -25,14 +28,24 @@ export default function FloatingButton({ scrollRef }: FloatingButtonProps) {
     target.scrollTo({ top: 0, behavior: 'smooth' });
   };
 
-  return (
-    show && (
-      <button
-        className="fixed bottom-30 right-10 w-14 h-14 rounded-full bg-orange-400 text-white text-2xl flex items-center justify-center shadow-lg hover:bg-amber-400 transition-all duration-500 animate-bounce z-50"
-        onClick={scrollToTop}
-      >
-        ↑
-      </button>
-    )
-  );
+  const renderFloatingButton = () => {
+    switch(type) {
+      case 'upscroll':
+        return (
+          show && (
+            <button
+              className="fixed bottom-30 right-10 w-14 h-14 rounded-full bg-orange-400 text-white text-2xl flex items-center justify-center shadow-lg hover:bg-amber-400 transition-all duration-500 animate-bounce z-50"
+              onClick={scrollToTop}
+            >
+              ↑
+            </button>
+          )
+        );
+      default:
+        return null;
+    }
+  };
+  
+  return <div>{renderFloatingButton()}</div>
 }
+
--- src/types/floatingbutton.ts
+++ src/types/floatingbutton.ts
@@ -1,5 +1,8 @@
 import { RefObject } from 'react';
 
+export type FloatingButtonType = 'upscroll' | 'darkmode' | 'end';
+
 export interface FloatingButtonProps {
+  type: FloatingButtonType;
   scrollRef?: RefObject<HTMLElement | null>;
 }

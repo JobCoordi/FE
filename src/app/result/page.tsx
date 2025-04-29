80a83be31e71e36bc810d601db780a9a7b8412a2
jaehyeongjung <titochung625@naver.com>
2025-04-29 09:30:25 +0900

    refactor: card- mockData 분리

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -1,11 +1,5 @@
 import Card from '@/components/common/Card';
-
-
-const cardData = [
-  { front: 'HTML', back: '하이퍼텍스트 마크업 언어' },
-  { front: 'CSS', back: '스타일 시트' },
-  { front: 'JavaScript', back: '동적인 웹을 만드는 언어' },
-];
+import {cardData} from '@/mock/resultCard';
 
 export default function Page() {
   return (
